f4905ca8c40990389b8f06f8aeccf8040cc491d5
juanlealr <juan.leal04@uptc.edu.co>
2026-09-20 12:04:42 -0500

    feat: esqueleto inicial completo del proyecto de red hospitalaria

---
+++ src/main/java/co/edu/uptc/red_hospitalaria/RedHospitalariaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.red_hospitalaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedHospitalariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedHospitalariaApplication.class, args);
+	}
+
+}
